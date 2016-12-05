978c25aa4ed29983970f21043248dddeaaa878c3
burza69 <burzabu@gmail.com>
2016-12-05 11:35:08 +0200

    Тестовый файлы

---
+++ test/test.php
@@ -0,0 +1,42 @@
+<!doctype html> 
+<html>
+
+<head>  
+<meta charset="utf-8"/>
+<!--<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> -->
+  
+ </head>
+
+<body>   
+
+
+
+<select name="food">
+
+<optgroup label="Вкусняшки">
+  
+  <option value="pie">Пирог</option> 
+  <option value="bread" selected="selected" label="Хлеб"></option> 
+   <option value="cookie" label="Печенье"></option>
+   </optgroup> 
+
+
+   <optgroup label="Пиво">
+
+  <option value="bear">123</option> 
+  <option value="bear2" selected="123">123</option> 
+   <option value="bear3" label="bear3">2343</option>
+   
+   </optgroup> 
+
+    
+    
+    </select>
+
+
+
+
+
+</body>
+
+</html
